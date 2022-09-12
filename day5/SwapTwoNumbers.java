aeee49964bf4254cb344d5db683be2d4c9494d1c
surajlangute <surajlangute55@gmail.com>
2022-09-12 12:55:41 +0530

    ADDED : Basic Core Programs from Day-5

--- day5/SwapTwoNumbers.java
+++ day5/SwapTwoNumbers.java
@@ -7,15 +7,15 @@ public class SwapTwoNumbers {
 	public static void main(String[] args) {
 		Scanner Input = new Scanner(System.in);
         System.out.print("Enter The Value of A : ");
-        int A = Input.nextInt();   //10
+        int A = Input.nextInt();   
         
         System.out.print("Enter The Value of B : ");
-        int B = Input.nextInt();   //20
+        int B = Input.nextInt();   
         
         System.out.println("Before Swaping " +A+ "," +B);
-        int temp = A; 				//temp = 10
-        A = B;   					// A = 20
-        B = temp;  					//10
+        int temp = A; 				
+        A = B;   					
+        B = temp;  					
         System.out.println("After Swaping " + A+ "," +B);
 	}
 

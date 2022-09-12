cff35ae3697f6b0763715a1442d6ad376a08f82f
surajlangute <surajlangute55@gmail.com>
2022-09-12 13:45:30 +0530

    ADDED : Basic Core Programs from Day-5

--- day5/PowerOfTwo.java
+++ day5/PowerOfTwo.java
@@ -1,9 +1,21 @@
 package basic.CorePrograms.day5;
 
+import java.util.Scanner;
+
 public class PowerOfTwo {
 
 	public static void main(String[] args) {
+		int num = 2;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Power value for 2");
+		int p = scan.nextInt();
+		int power = 1;
 
+		for (int i = 1; i <= p; i++) {
+			power = power * num;
+		}
+
+		System.out.println("2 to the power "+ p +" is : " + power);
 	}
 
 }
